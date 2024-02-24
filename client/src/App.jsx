8e818b513f97a5a004d7450c0f0b9f4afcf1722e
DamiansNase <damian.bergemann.privat@gmail.com>
2024-02-24 20:08:00 +0100

    Create the client site

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
+      
+  )
+}
+
+export default App
