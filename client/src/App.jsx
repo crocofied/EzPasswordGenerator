1dfa0c9fe3f11921302f9a6edc89cd86df2f3fa2
DamiansNase <damian.bergemann.privat@gmail.com>
2024-02-24 20:29:11 +0100

    Added Basic Styling to the App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import './index.css'
 
 function App() {
-  const [displayPassword, setDisplayPassword] = useState('')
+  const [displayPassword, setDisplayPassword] = useState('Click the button to generate a password')
 
   const generatePassword = () => {
     axios.get('http://localhost:8000/password')
@@ -14,11 +14,15 @@ function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      EzPasswordGenerator
-    </h1>
-    <button onClick={generatePassword}>Generate a Password</button>
-    <p>{displayPassword}</p>
+    <div className='flex flex-col items-center'>
+      <h1 className="text-3xl font-bold text-slate-800 p-4">
+        EzPasswordGenerator
+      </h1>
+      <button className="font-bold rounded bg-transparent border border-slate-700 hover:bg-slate-700 text-slate-700 hover:text-white py-2 px-2" onClick={generatePassword}>Generate a Password</button>
+      <div className='pt-5'>
+        <p className='border border-slate-700 p-2'>{displayPassword}</p>
+      </div>
+    </div>
     </>
       
   )
