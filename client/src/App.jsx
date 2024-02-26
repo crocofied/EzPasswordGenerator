3085644a7fb9940b54846105f8a982ceeeed8011
DamiansNase <damian.bergemann.privat@gmail.com>
2024-02-26 21:50:12 +0100

    Fixed display of unreleased feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,11 +25,6 @@ function App() {
         EzPasswordGenerator
       </h1>
 
-      <label class="inline-flex items-center cursor-pointer">
-        <input type="checkbox" value="" class="sr-only peer"/>
-        <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-        <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Dark Mode</span>
-      </label>
 
       {/* Input Values and Button */}
       <div className="pt-5 border w-1/3 border-slate-700 flex flex-col items-center">
